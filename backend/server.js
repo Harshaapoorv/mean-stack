8f06c72e457c7dfbd98ee81fd112d6b4057f342b
Harshaapoorv <harshaapoorva164@gmail.com>
2024-09-29 23:36:49 +0530

    Basic Express server

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,8 @@
 // Nodemon -- Watches the changes in server.js so everytime we don't have to restart the server
-console.log("Hello, World")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 4733
+
+const app = express()
+
+app.listen(port, () => console.log(`Server running at port: ${port}`))
